21d423a1745b2ba664875cf24a774303b0e31453
inatahhh <itsmearani123@live.ca>
2024-10-12 15:54:45 -0400

    Brouillon

--- TP2_A24V2/Ordre_lexicographique/comparaison.s
+++ TP2_A24V2/Ordre_lexicographique/comparaison.s
@@ -5,6 +5,8 @@ length1:
 .int 4
 string2:
 .ascii "test1" # Si vous voulez tester l'égalité, mettez la string la plus longue dans string2
+                # La longeur doit etre la meme
+                # Les characteres sont les memes [ASCII] (0x61 à 0x7a minuscule) et (0x41 à 0x5a majuscules)
 length2:
 .int 5
 
@@ -12,7 +14,17 @@ length2:
 .globl comparaison
 
 main : 
+push ebp
+mov ebp, esp
+sub esp, 4
+
+lea string1, %esi # l'addresse de string1 est mis dans le registre %esi
+lea string2, %edi # l'addresse de string2 est mis dans le registre %edi
+mov ecx, length1
+
 comparaison : 
+
+cmps # Compare la valeur de la chaine en memoire d'adresse %esi avec la valeur de la chaine en memoire d'adresse %edi
 
 ret
 push %eax
